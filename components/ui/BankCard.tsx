164817571811786caf414713740c424b37ce6189
daniel.stark@global.komatsu <daniel.stark@global.komatsu>
2024-07-19 15:08:59 -0700

    Implementing the my banks page

--- components/ui/BankCard.tsx
+++ components/ui/BankCard.tsx
@@ -1,12 +1,14 @@
 import { formatAmount } from '@/lib/utils'
+
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import Copy from './copy'
 
 const BankCard = ({account, userName, showBalance = true}: CreditCardProps) => {
   return (
     <div className='flex flex-col'>
-        <Link href="/" className='bank-card'>
+        <Link href={`/transaction-history/?id=${account.appwriteItemId}`} className='bank-card'>
             <div className='bank-card_content'>
                 <div>
                     <h1 className='text-16 font-semibold text-white'>
@@ -26,7 +28,7 @@ const BankCard = ({account, userName, showBalance = true}: CreditCardProps) => {
                         </h2>
                     </div>
                         <p className='text-14 font-semibold tracking-[1.1] text-white'>
-                        ●●●● ●●●● ●●●● <span className='text-16'> {1234}</span>
+                        ●●●● ●●●● ●●●● <span className='text-16'> {account?.mask}</span>
                         </p>
                 </article>
             </div>
@@ -55,6 +57,9 @@ const BankCard = ({account, userName, showBalance = true}: CreditCardProps) => {
 
             </div>
         </Link>
+
+        {showBalance && <Copy title={account?.shareableId}/>}
+
     </div>
   )
 }
